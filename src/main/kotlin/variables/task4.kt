fb611fb47020f0e8d0612ee00f92b61966ce03a4
Alexander Kobelev <kobelef@mail.ru>
2024-09-21 23:05:23 +0400

    KS-variables-4

---
+++ src/main/kotlin/variables/task4.kt
@@ -0,0 +1,11 @@
+package variables
+
+fun main() {
+    val length: Long = 40868600000
+    val age:Byte = 27
+    val  partOfDay:Double = 0.075
+    val  seconds:Int = 648
+    val partOfYear: Float = 2.0547945205479453E-4F
+    val apogee: Int = 327000
+
+}
